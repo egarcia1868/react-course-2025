785b15e9ffb8a98e493fbd0f1e293cf8a068ac14
Eric Garcia <egarcia1868@gmail.com>
2025-01-09 18:20:31 -0500

    finished lesson 44

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
-import reactImg from './assets/react-core-concepts.png';
+import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from './assets/components.png';
 
 const reactDescriptions = ["Fundamental", "Crucial", "Core"];
 
@@ -12,10 +13,20 @@ function Header() {
       <img src={reactImg} alt="Stylized atom" />
       <h1>React Essentials</h1>
       <p>
-        {reactDescriptions[genRandomInt(2)]} React concepts you will need for almost any app you are
-        going to build!
+        {reactDescriptions[genRandomInt(2)]} React concepts you will need for
+        almost any app you are going to build!
       </p>
     </header>
+  );
+}
+
+function CoreConcept(props) {
+  return (
+    <li>
+      <img src={props.img} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
   );
 }
 
@@ -24,7 +35,19 @@ function App() {
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title="Components"
+              description="The core UI building block"
+              img={componentsImg}
+            />
+            <CoreConcept />
+            <CoreConcept />
+            <CoreConcept />
+          </ul>
+        </section>
       </main>
     </div>
   );
