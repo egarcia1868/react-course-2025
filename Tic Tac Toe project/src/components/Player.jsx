eaecbf604600fd7444bbe670dee21e1d368f124c
Eric Garcia <egarcia1868@gmail.com>
2025-01-11 14:52:44 -0500

    finished lesson 78

--- Tic Tac Toe project/src/components/Player.jsx
+++ Tic Tac Toe project/src/components/Player.jsx
@@ -2,15 +2,18 @@ import { useState } from "react";
 
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
+//   const [playerName, setPlayerName] = useState(name);
+let btnCaption = 'Edit'
 
   function handleEditClick() {
-    setIsEditing(true);
+    setIsEditing((editing) => !editing);
   }
 
   let playerName = <span className="player-name">{name}</span>;
 
   if (isEditing) {
-    playerName = <input type="text" required />;
+    playerName = <input type="text" required value={name} />;
+    btnCaption = 'Save'
   }
 
   return (
@@ -19,7 +22,7 @@ export default function Player({ name, symbol }) {
         {playerName}
         <span className="player-symbol">O</span>
       </span>
-      <button onClick={handleEditClick}>Edit</button>
+      <button onClick={handleEditClick}>{btnCaption}</button>
     </li>
   );
 }
