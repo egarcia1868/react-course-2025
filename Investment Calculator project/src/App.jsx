4d7bc5a8857532d2d3203f680b7ea81bcfb648d5
Eric Garcia <egarcia1868@gmail.com>
2025-01-13 20:48:54 -0500

    finished lesson 103.  NYW

--- Investment Calculator project/src/App.jsx
+++ Investment Calculator project/src/App.jsx
@@ -4,13 +4,15 @@ import Results from "./components/Results";
 import UserInput from "./components/UserInput";
 
 function App() {
-  const [ investmentResults, setInvestmentResults ] = useState([])
+  const [investmentResults, setInvestmentResults] = useState([]);
   const [userInput, setUserInput] = useState({
     initialInvestment: 10000,
     annualInvestment: 1200,
     expectedReturn: 6,
     duration: 10,
   });
+
+  const inputIsValid = userInput.duration > 0;
 
   function handleChange(inputIdentifier, newValue) {
     setUserInput((prevUserInput) => {
@@ -23,11 +25,15 @@ function App() {
 
   return (
     <>
-    <Header />
-      <UserInput onChange={handleChange} userInput={userInput}/>
-      <Results input={userInput} />
+      <Header />
+      <UserInput onChange={handleChange} userInput={userInput} />
+      {inputIsValid ? (
+        <Results input={userInput} />
+      ) : (
+        <p className="center">Please enter a duration greater than 0.</p>
+      )}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
