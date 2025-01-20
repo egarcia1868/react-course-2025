08f2be802241aae9adeb3183f9553ff888a7ec03
Eric Garcia <egarcia1868@gmail.com>
2025-01-20 16:48:17 -0500

    lesson 120. using tailwind

--- ReactArt/tailwind.config.js
+++ ReactArt/tailwind.config.js
@@ -4,7 +4,11 @@ export default {
     './index.html', './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['"Pacifico"', 'cursive']
+      }
+    },
   },
   plugins: [],
 }
