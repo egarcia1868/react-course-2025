27015c14d923a88113272cfd8871d0ae081c98d9
Eric Garcia <egarcia1868@gmail.com>
2025-01-23 20:44:57 -0500

    156. adding new modal

--- Practice project 2/src/App.jsx
+++ Practice project 2/src/App.jsx
@@ -5,17 +5,10 @@ import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 
 function App() {
-  const [ projects, setProjects ] = useState([]);
-  const [ currentProject, setCurrentProject ] = useState(null);
-  const [ addProject, setAddProject ] = useState(false);
   const [ projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
     projects: []
   });
-
-  // function handleSaveProjects(newProject) {
-  //   setProjects([...projects, newProject])
-  // }
 
   function handleStartAddProject() {
     setProjectsState(prevState => {
@@ -51,23 +44,11 @@ function App() {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
   }
 
-  // function handleCloseNewProject() {
-  //   setAddProject(false);
-  // }
-
-  // function handleOpenProject(selectedProject) {
-  //   setCurrentProject(selectedProject)
-  // }
-
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectsSidebar projects={projectsState.projects} onStartAddProject={handleStartAddProject}/>
-      {/* <ProjectsSidebar onStartAddProject={handleStartAddProject} openProject={handleOpenProject} projects={projects} addNewProject={handleStartAddProject}/> */}
       {content}
-      {/* {addProject && <NewProject onSubmit={handleSaveProjects} projects={projects} onCancel={handleCloseNewProject}/>} */}
-      {/* {!currentProject ? <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1> :  */}
-      {/* <Project project={currentProject} />} */}
-    </main>
+      </main>
   );
 }
 
--- Practice project 2/src/components/NewProject.jsx
+++ Practice project 2/src/components/NewProject.jsx
@@ -1,67 +1,66 @@
 import { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
-export default function NewProject({ onAdd, onSubmit, projects, onCancel }) {
-//   const [newProject, setNewProject] = useState({
-//     title: "",
-//     description: "",
-//     date: "",
-//   });
-//   const saveEnabled =
-//     newProject.title && newProject.description && newProject.date;
+export default function NewProject({ onAdd, onCancel }) {
+  const modal = useRef();
 
-//   function handleChange(inputIdentifier, newValue) {
-//     // const project = newProject;
-//     // project[property] = e.target.value
-//     setNewProject((prevProjectInfo) => {
-//       return {
-//         ...prevProjectInfo,
-//         [inputIdentifier]: newValue,
-//       };
-//     });
-//   }
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
 
-//   function handleFormSubmit(e) {
-//     e.preventDefault();
-//     onSubmit(newProject);
-//     onCancel();
-//   }
-
-const title = useRef();
-const description = useRef();
-const dueDate = useRef();
-
-function handleSave() {
+  function handleSave() {
     const enteredTitle = title.current.value;
     const enteredDescription = description.current.value;
     const enteredDueDate = dueDate.current.value;
 
+    if (
+      enteredTitle.trim() === "" ||
+      enteredDescription.trim() === "" ||
+      enteredDueDate.trim() === ""
+    ) {
+      modal.current.open();
+      return;
+    }
     onAdd({
-        title: enteredTitle,
-        description: enteredDescription,
-        dueDate: enteredDueDate
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
     });
-}
-
-
+  }
 
   return (
-    <div className="w-[35rem] mt-16" >
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button className="text-stone-800 hover:text-stone-950" onClick={onCancel}>Cancel</button>
-        </li>
-        <li>
-          <button onClick={handleSave} className='bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md'>
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input ref={title} label="Title" />
-        <Input ref={description} label="Description" textarea />
-        <Input ref={dueDate} label="Due Date" type='date' />
+    <>
+      <Modal ref={modal} buttonCaption="Okay">
+        <h2>Invalid Input</h2>
+        <p>Oops ... looks like you forgot to enter a value.</p>
+        <p>Please make sure you provide a valid value for every input field.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleSave}
+              className="bg-stone-800 text-stone-50 hover:bg-stone-950 px-6 py-2 rounded-md"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input ref={title} label="Title" />
+          <Input ref={description} label="Description" textarea />
+          <Input ref={dueDate} label="Due Date" type="date" />
+        </div>
       </div>
-    </div>
+    </>
   );
 }
