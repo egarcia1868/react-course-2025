3d6609e255e386817cb389ec68a58dc3833b6021
Eric Garcia <egarcia1868@gmail.com>
2025-01-29 04:41:32 -0500

    adding previously left off file updates

--- Practice project 2/src/components/NewTask.jsx
+++ Practice project 2/src/components/NewTask.jsx
@@ -1,18 +1,18 @@
 import { useState } from "react";
 
-export default function NewTask({onAdd}) {
-  const [enteredTask, setEnteredTask] = useState('');
+export default function NewTask({ onAdd }) {
+  const [enteredTask, setEnteredTask] = useState("");
 
   function handleChange(e) {
     setEnteredTask(e.target.value);
   }
 
   function handleClick() {
-    if (enteredTask.trim() === '') {
-     return; 
+    if (enteredTask.trim() === "") {
+      return;
     }
     onAdd(enteredTask);
-    setEnteredTask('');
+    setEnteredTask("");
   }
 
   return (
