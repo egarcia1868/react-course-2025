8bf4338ad89b4917a6c603dd6ca8c8df0377e077
Eric Garcia <egarcia1868@gmail.com>
2025-01-29 04:40:26 -0500

    181

--- Side Effects - useEffect project/src/App.jsx
+++ Side Effects - useEffect project/src/App.jsx
@@ -20,9 +20,9 @@ function App() {
         position.coords.latitude,
         position.coords.longitude
       );
-  
+
       setAvailablePlaces(sortedPlaces);
-    })
+    });
   }, []);
 
   function handleStartRemovePlace(id) {
@@ -42,6 +42,14 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem("selectedPlaces")) || [];
+    if (storedIds.indexOf(id) === -1) {
+      localStorage.setItem(
+        "selectedPlaces",
+        JSON.stringify([id, ...storedIds])
+      );
+    }
   }
 
   function handleRemovePlace() {
@@ -78,7 +86,7 @@ function App() {
         <Places
           title="Available Places"
           places={availablePlaces}
-          fallbackText='Sorting places by distance...'
+          fallbackText="Sorting places by distance..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
