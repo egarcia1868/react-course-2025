fff176e4191013bfbdf6f11a084795a3129d7d25
Eric Garcia <egarcia1868@gmail.com>
2025-02-20 17:32:21 -0500

    210 - using useCallback to prevent unnecessary recreating of functions

--- React Behind the Scenes/src/components/Counter/Counter.jsx
+++ React Behind the Scenes/src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import { memo, useState } from "react";
+import { memo, useState, useCallback } from "react";
 
 import IconButton from "../UI/IconButton.jsx";
 import MinusIcon from "../UI/Icons/MinusIcon.jsx";
@@ -23,19 +23,20 @@ function isPrime(number) {
   return true;
 }
 
-export default function Counter({ initialCount }) {
+// memo no longer serving much of a purpose below.  Just leaving it to show how it can be used.
+const Counter = memo(function Counter({ initialCount }) {
   log("<Counter /> rendered", 1);
   const initialCountIsPrime = isPrime(initialCount);
 
   const [counter, setCounter] = useState(initialCount);
 
-  function handleDecrement() {
+  const handleDecrement = useCallback(function handleDecrement() {
     setCounter((prevCounter) => prevCounter - 1);
-  }
+  }, []);
 
-  function handleIncrement() {
+  const handleIncrement = useCallback(function handleIncrement() {
     setCounter((prevCounter) => prevCounter + 1);
-  }
+  }, []);
 
   return (
     <section className="counter">
@@ -54,4 +55,6 @@ export default function Counter({ initialCount }) {
       </p>
     </section>
   );
-};
+});
+
+export default Counter;
