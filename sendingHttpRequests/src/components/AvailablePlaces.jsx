389b8cc730426301a5c77fd38fa0d07daba420f8
Eric Garcia <egarcia1868@gmail.com>
2025-02-25 16:14:20 -0500

    Starting code for Section 15

---
+++ sendingHttpRequests/src/components/AvailablePlaces.jsx
@@ -0,0 +1,16 @@
+import Places from './Places.jsx';
+
+export default function AvailablePlaces({ onSelectPlace }) {
+  const [ availablePlaces, setAvailablePlaces ] = useState([]);
+
+  
+
+  return (
+    <Places
+      title="Available Places"
+      places={availablePlaces}
+      fallbackText="No places available."
+      onSelectPlace={onSelectPlace}
+    />
+  );
+}
